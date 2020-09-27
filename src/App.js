f69bf978169d9f354bdd924928a77131b99c4efd
Jagjeet Singh <sjagjeet528@gmail.com>
2020-09-27 13:13:55 +0530

    setup context api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import SpotifyWebApi from 'spotify-web-api-js';
+
 import './App.css';
+
 import Login from './Login';
+import Player from './Player';
 import { getTokenFromUrl } from "./spotify";
-import SpotifyWebApi from 'spotify-web-api-js'
 
 const spotify =  new SpotifyWebApi();
 
@@ -10,6 +13,7 @@ function App() {
   const [token, setToken] = useState(null);
 
   useEffect(() => {
+   
     const hash = getTokenFromUrl(); 
     window.location.hash = "";
     const _token = hash.access_token;
@@ -27,7 +31,7 @@ function App() {
   return (
     <div className="App">
       {
-        token ? <h1>I have token</h1> : <Login />
+        token ? <Player /> : <Login />
       }
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { DataLayer } from './DataLayer';
+import reducer, { initialState } from './reducer';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DataLayer initialState={initialState} reducer={reducer}>
+      <App />
+    </DataLayer>
   </React.StrictMode>,
   document.getElementById('root')
 );
